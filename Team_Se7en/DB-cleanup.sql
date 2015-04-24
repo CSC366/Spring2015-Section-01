d60a6d726c2d170bdb70be57491c8aa4eb303b98
mlinsenbard <mlinsenb@calpoly.edu>
2015-04-24 11:48:51 -0700

    Renaming files

---
+++ Team_Se7en/DB-cleanup.sql
@@ -0,0 +1,17 @@
+DROP TABLE RegSources;
+
+DROP TABLE URLs;
+
+DROP TABLE Devices;
+
+DROP TABLE Customers; 
+
+DROP TABLE Emails;
+
+DROP TABLE Registrations;
+
+DROP TABLE Campaigns;
+
+DROP TABLE Messages;
+
+DROP TABLE Events;

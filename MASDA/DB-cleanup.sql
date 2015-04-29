ae0ed1b87a97e0529bb2884134f1b6ec4d36bd43
Steven <steventhon94@gmail.com>
2015-04-29 11:41:12 -0700

    Update DB-cleanup.sql

--- MASDA/DB-cleanup.sql
+++ MASDA/DB-cleanup.sql
@@ -1,11 +1,12 @@
 -- Database schema Delete tables 1
 -- Group Masda cpe 366-01
 
-drop table Events;
-drop table EventTypes;
+drop table Clicked;
 drop table Links;
-drop table EmailInstances;
-drop table Campaigns;
+drop table Generate;
+drop table EventTypes;
+drop table Recieve;
+drop table Messages;
 drop table EmailAddresses;
 drop table Stores;
 drop table Registrations;

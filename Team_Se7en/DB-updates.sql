b363c75e1c04a0b9d5039c301901baafca5828af
mversagg <mversagg@calpoly.edu>
2015-05-13 02:47:22 -0700

    Update DB-updates.sql

--- Team_Se7en/DB-updates.sql
+++ Team_Se7en/DB-updates.sql
@@ -11,43 +11,58 @@ INSERT INTO RegSrcs (RegSrcID, Name)
 -- remove bad data
 DELETE FROM tempModels
 WHERE Name = '';
--- insert
+-- insert the devices found in both Device.csv and Device_Model.csv
 INSERT INTO Devices (Model, Name, Type, Carrier)
-   SELECT DISTINCT Model, Name, Type, Carrier
-   FROM tempModels
+SELECT DISTINCT Model, Name, Type, Carrier
+FROM tempModels;
+-- insert the devices found only in Device.csv
+INSERT INTO Devices (Model, Name, Type, Carrier)
+SELECT DISTINCT Model, null, null, null
+FROM tempDevices
+WHERE Model NOT IN (SELECT DISTINCT Model
+                    FROM tempModels)
 ;
 
--- populating Customers
+-- populating Customers that have at least 1 registered device
 INSERT INTO Customers (CustomerID, Zip, State, Gender, Income,
    Permission, Language, RegDate, Tier, NumRegs, RegSrcID)
-   SELECT DISTINCT Ac.CustomerId, Ac.Zip, Ac.State, Ac.Gender, Ac.Income,
-          Ac.Permission, Ac.Language, Ac.RegDate, Ac.Tier,
-          D.numReg, Ac.RegSrcID
-   FROM (SELECT DISTINCT CustomerID, Zip, State, Gender, Income,
-                         Permission, Language, RegDate, Tier, RegSrcID
-         FROM tempAccounts
-         GROUP BY CustomerID) Ac,
-        (SELECT DISTINCT CustomerID, numReg
-         FROM tempDevices) D
-   WHERE Ac.CustomerID = D.CustomerID
-      OR Ac.CustomerID NOT IN (SELECT DISTINCT CustomerID
-                               FROM tempDevices)
+SELECT DISTINCT Ac.CustomerID, Ac.Zip, Ac.State, Ac.Gender, Ac.Income,
+   Ac.Permission, Ac.Language, Ac.RegDate, Ac.Tier,
+   D.numReg, Ac.RegSrcID
+FROM (SELECT DISTINCT CustomerID, Zip, State, Gender, Income,
+         Permission, Language, RegDate, Tier, RegSrcID
+      FROM tempAccounts
+      GROUP BY CustomerID) Ac,
+     (SELECT DISTINCT CustomerID, numReg
+      FROM tempDevices) D
+WHERE Ac.CustomerID = D.CustomerID
+;
+
+-- populating Customers that have no registered devices
+INSERT INTO Customers (CustomerID, Zip, State, Gender, Income,
+   Permission, Language, RegDate, Tier, NumRegs, RegSrcID)
+SELECT DISTINCT CustomerID, Zip, State, Gender, Income, Permission, Language,
+   RegDate, Tier, 0, RegSrcID
+FROM tempAccounts
+WHERE CustomerID NOT IN (SELECT DISTINCT CustomerID
+                         FROM tempDevices)
+GROUP BY CustomerID
 ;
 
 -- populating Registrations
 INSERT INTO Registrations (RegID, PurchaseDate, PurchaseStoreName,
    PurchaseStoreState, PurchaseStoreCity, Ecomm, Serial, Model,
    RegDate, CustomerID, RegSrcID)
-      SELECT DISTINCT RegID, PurchaseDate, PurchaseStoreName, PurchaseStoreState,
-             PurchaseStoreCity, Ecomm, Serial, Model, RegDate,
-             CustomerID, RegSrcID
-      FROM tempDevices
+SELECT DISTINCT RegID, PurchaseDate, PurchaseStoreName, PurchaseStoreState,
+   PurchaseStoreCity, Ecomm, Serial, Model, RegDate,
+   CustomerID, RegSrcID
+FROM tempDevices
 ;
 
 -- populating Emails
 INSERT INTO Emails (EmailID, Domain, CustomerID)
-   SELECT DISTINCT EmailID, Domain, CustomerID
-   FROM tempAccounts
+SELECT DISTINCT EmailID, Domain, CustomerID
+FROM tempAccounts
 ;
 
 -- populating Campaigns
@@ -57,49 +72,51 @@ DROP COLUMN EmailID2
 ;
 
 INSERT INTO Campaigns (Name)
-   SELECT DISTINCT Campaign
-   FROM tempEmails
+SELECT DISTINCT Campaign
+FROM tempEmails
 ;
 
 -- populating Messages
 INSERT INTO Messages (DeployID, DeployDate, Subject, Version, Audience, CampaignID, EmailID)
-      SELECT DISTINCT DeployID, DeployDate, Subject, Version, Audience, CampaignID, EmailID
-      FROM tempEmails, Campaigns
-      WHERE tempEmails.Campaign = Campaigns.Name
+SELECT DISTINCT DeployID, DeployDate, Subject, Version, Audience, CampaignID, EmailID
+FROM tempEmails, Campaigns
+WHERE tempEmails.Campaign = Campaigns.Name
 ;
 
 -- populating Links
 INSERT INTO Links (URL, LinkName, MsgID)
-   SELECT DISTINCT URL, LinkName, MsgID
-   FROM tempEmails T, Messages M, Campaigns C
-   WHERE C.Name = M.Campaign
-     AND T.DeployID = M.DeployID
-     AND T.DeployDate = M.DeployDate
-     AND T.EmailID = M.EmailID
-     AND T.Audience = M.Audience
-     AND T.Version = M.Version
-     AND T.Subject = M.Subject
-     AND T.Campaign = M.Campaign
+SELECT DISTINCT URL, LinkName, MsgID
+FROM tempEmails T, Messages M, Campaigns C
+WHERE C.CampaignID = M.CampaignID
+AND T.DeployID = M.DeployID
+AND T.DeployDate = M.DeployDate
+AND T.EmailID = M.EmailID
+AND T.Audience = M.Audience
+AND T.Version = M.Version
+AND T.Subject = M.Subject
+AND T.Campaign = C.Name
+AND URL != ''
 ;
 
 -- populating EventTypes
 INSERT INTO EventTypes (EventID, Name)
-   SELECT DISTINCT EventID, EventName
-   FROM tempEmails
+SELECT DISTINCT EventID, EventName
+FROM tempEmails
 ;
 
 -- populating Events
-INSERT INTO Events (EventID, MsgID, EventID, LinkID, EmailEventDateTime)
-   SELECT EventID, MsgID, EventID, LinkID, EmailEventDateTime
-   FROM tempEmails T, Messages M, Links L, EventTypes E, Campaigns C
-   WHERE C.Name = M.Campaign
-     AND T.DeployID = M.DeployID
-     AND T.DeployDate = M.DeployDate
-     AND T.EmailID = M.EmailID
-     AND T.Audience = M.Audience
-     AND T.Version = M.Version
-     AND T.Subject = M.Subject
-     AND T.Campaign = M.Campaign
-     AND T.EventID = E.EventID
-     AND L.MsgID = M.MsgID
+-- This should cause 4783 Duplicates/Warnings
+INSERT IGNORE INTO Events (MsgID, EventID, LinkID, EmailEventDateTime)
+SELECT M.MsgID, E.EventID, LinkID, EmailEventDateTime
+FROM tempEmails T, Messages M, Links L, EventTypes E, Campaigns C
+WHERE C.CampaignID = M.CampaignID
+AND T.DeployID = M.DeployID
+AND T.DeployDate = M.DeployDate
+AND T.EmailID = M.EmailID
+AND T.Audience = M.Audience
+AND T.Version = M.Version
+AND T.Subject = M.Subject
+AND T.Campaign = C.Name
+AND T.EventID = E.EventID
+AND L.MsgID = M.MsgID
 ;

db035be41f92cfe51d0fcded1fb4c6daf948fea9
mversagg <mversagg@calpoly.edu>
2015-05-17 18:45:40 -0700

    Create DB-WH_cleanup.sql

---
+++ Team_Se7en/DB-WH_cleanup.sql
@@ -0,0 +1,8 @@
+/* 
+ *  Cleanup for Rosetta Project Data Warehouse
+ */
+ 
+DROP TABLE IF EXISTS EventData;
+DROP TABLE IF EXISTS CustomerData;
+DROP TABLE IF EXISTS DeviceRegData;
+DROP TABLE IF EXISTS EmailData;

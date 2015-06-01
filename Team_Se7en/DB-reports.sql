f65bf33a810c43361808135575f3feefe431a5ce
mversagg <mversagg@calpoly.edu>
2015-06-01 11:24:03 -0700

    Update DB-reports.sql

--- Team_Se7en/DB-reports.sql
+++ Team_Se7en/DB-reports.sql
@@ -2,10 +2,22 @@
 SELECT CampaignName, Audience, Version, Subject, DeployDate, UniqueDelivers, UniqueOpens, UniqueClicks,
    (UniqueOpens / UniqueDelivers) AS OpenRate, (UniqueClicks / UniqueOpens) AS ClickToOpenRate,
    (UniqueClicks / UniqueDelivers) AS ClickRate, (UniqueUnsubs / UniqueOpens) AS UnsubRate
-FROM EmailData;
+FROM EmailData
+INTO OUTFILE '~/366/report1.csv'
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n';
 
 -- Report 2
-SELECT * FROM CustomerData;
+SELECT * FROM CustomerData
+INTO OUTFILE '~/366/report2.csv'
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n';
 
 -- Report 3
-SELECT * FROM DeviceRegData;
+SELECT * FROM DeviceRegData
+INTO OUTFILE '~/366/report3.csv'
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n';

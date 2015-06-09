232b9d695fa5337cc65469e3279bb6ed4d4c5090
Esha Joshi <eshajoshi@gmail.com>
2015-06-09 16:06:27 -0700

    data warehouses with the order by

--- SELECT*FROMJoPo;/DB-reports.sql
+++ SELECT*FROMJoPo;/DB-reports.sql
@@ -1,3 +1,3 @@
-SELECT * FROM EmailCampaignPerformance;
-SELECT * FROM AccountRegistrationReport;
-SELECT * FROM DeviceRegistrationReport;
+SELECT * FROM EmailCampaignPerformance ORDER BY `Campaign Name`,Audience,Version,`Subject Line`,`Deployment Date`;
+SELECT * FROM AccountRegistrationReport ORDER BY `MONTHNAME(CustomerAccount.regDate)`,`YEAR(CustomerAccount.regDate)`;
+SELECT * FROM DeviceRegistrationReport ORDER BY `MONTHNAME(DeviceRegistration.registration_date)`,`YEAR(DeviceRegistration.registration_date)`;

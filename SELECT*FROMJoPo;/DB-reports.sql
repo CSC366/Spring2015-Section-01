944d6ccc1e41b4fc34db39cf4962ee4403aa2c64
dchan17 <dchan17@calpoly.edu>
2015-06-09 12:58:52 -0700

    FinalReport.pdf

--- SELECT*FROMJoPo;/DB-reports.sql
+++
@@ -1,3 +0,0 @@
-SELECT * FROM EmailCampaignPerformance;
-SELECT * FROM AccountRegistrationReport;
-SELECT * FROM DeviceRegistrationReport;
--- SELECT*FROMJoPo;/Scripts/ClickedLink.py
+++
@@ -1,7 +0,0 @@
-class ClickedLink:
-   def __init__(self, event_id, link_id):
-      self.fk_event_id = event_id
-      self.fk_link_id = link_id
-
-   def __str__(self):
-      return str(self.fk_event_id) + ', ' + str(self.fk_link_id)
